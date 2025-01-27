694ee191c991a160eda6104d55e59843b103f413
Saumya Shrivastava <76044767+smy14r@users.noreply.github.com>
2025-01-28 00:15:30 +0530

    Create app without DB

---
+++ src/main/java/com/smy14r/journalApp/controller/HealthCheck.java
@@ -0,0 +1,13 @@
+package com.smy14r.journalApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    @GetMapping("/health-check")
+    public String healthCheck(){
+        return "Ok";
+    }
+}
---
+++ src/main/java/com/smy14r/journalApp/entity/JournalEntry.java
@@ -0,0 +1,33 @@
+package com.smy14r.journalApp.entity;
+
+public class JournalEntry {
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    private String title;
+
+    private String content;
+}
